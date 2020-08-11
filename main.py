7adb3bdf8de4a81615b2cd29afa8d51edd86d4e8
Emanuel Ciuca <emanuel.ciuca@gmail.com>
2020-08-11 17:17:12 +0300

    Generous bot

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import tweepy
+import time
 
 auth = tweepy.OAuthHandler('api key', 'consumer secret')
 auth.set_access_token('access_token', 'access_token_secret')
 
 api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
+
+def limit_handler(cursor):
+    try:
+        while True:
+            yield cursor.next()
+    except tweepy.RateLimitError:
+        time.sleep(1000)
+
+
+# Generous bot
+for follower in limit_handler(tweepy.Cursor(api.followers).items()):
+    if follower.name == 'Some name':
+        print(follower.name)
+        follower.follow()
+        break
